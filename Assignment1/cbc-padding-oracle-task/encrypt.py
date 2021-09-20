a06031a6866baed81cd796586f5c48bbe38d6294
Simon Wessmann Brok <simon.wessmann.brok@gmail.com>
2021-09-20 16:05:55 +0200

    wuhuu

--- Assignment1/cbc-padding-oracle-task/encrypt.py
+++ Assignment1/cbc-padding-oracle-task/encrypt.py
@@ -1,7 +1,7 @@
 import requests
-import secrets
+from Crypto.Util.Padding import pad
 
-basepath = 'http://localhost:5000'
+
 basepath = 'http://localhost:5000'
 quotepath = basepath + '/quote'
 
@@ -11,43 +11,60 @@ def askPaddingOracle(text):
  cookies = {'authtoken': text.hex()}
  response = requests.get(quotepath, cookies = cookies)
  return response
+
+def askForQuote(text):
+ cookies = {'authtoken': text.hex()}
+ response = requests.get(quotepath, cookies = cookies)
+ print(response.text)
+ return response
  
+def encryptBlock(prev, next):
+
+   CipherBytes =  bytearray(16)
+   dBlock = bytearray(16)
+   cMarked = bytearray(16)
+   
+
+   for byteindex in range (15,-1, -1): 
+      paddingvalue = 16 - byteindex
+      for index in range(15, byteindex, -1):
+         cMarked[index] = paddingvalue ^ dBlock[index]
+     
+      for value in range(0,256):
+         cMarked[byteindex] = value
+         blockToSend = cMarked + prev
+         
+         response = askPaddingOracle(blockToSend)
+         
+         if 'padding is incorrect' not in response.text.lower():
+            dByteValue = value ^ paddingvalue
+            dBlock[byteindex] = dByteValue
+            cipherByte = next[byteindex] ^ paddingvalue ^ value
+            CipherBytes[byteindex] = cipherByte
+            print(str(cipherByte))
+            break
+         else:
+            continue
+
+   return CipherBytes
+
+ 
+
 def main():
- encodedText = textToEncrypt.encode()
- textByteArray = bytearray(encodedText)
- 
- plainTextArray = bytearray(48)
- 
- for index in range(35, 48):
-  plainTextArray[index] = 13
- 
- for index in range (0, 35):
-  plainTextArray[index] = textByteArray[index]
- 
- cipherN = bytearray(16)
- 
- p1 = bytearray(plainTextArray[0:16])
- p2 = bytearray(plainTextArray[16:32])
- p3 = bytearray(plainTextArray[32:48])
- 
- intermediate = bytearray(16)
- emptyBlock = bytearray(16)
- 
- for value in range(0,255):
-  emptyBlock[15] = value
-  Cdot = emptyBlock + cipherN
-  response = askPaddingOracle(Cdot)
-  if 'padding is incorrect' not in response.text.lower():
-   plaintextValue = p3[15]
-   print(value)
-   print(plaintextValue)
-   print(value ^ 1 ^ plaintextValue)
-   intermediate[15] = value ^ 1 ^ plaintextValue
-   break
-  else:
-   continue
- 
- 
+   encodedText = textToEncrypt.encode()
+   textByteArray = bytearray(encodedText)
+   plainTextArray = pad(textByteArray, 16)
+
+   p1 = bytearray(plainTextArray[0:16])
+   p2 = bytearray(plainTextArray[16:32])
+   p3 = bytearray(plainTextArray[32:48])
+
+   c3 = bytearray(16)
+   c2 = encryptBlock(c3, p3)
+   c1 = encryptBlock(c2, p2)
+   iv = encryptBlock(c1, p1)
+   finalCipher = iv + c1 + c2 + c3
+   askForQuote(finalCipher)   
  
 if __name__ == "__main__":
  main()
