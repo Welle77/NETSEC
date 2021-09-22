d7bdea3ecf1ab4e72d5edfdf343eb7aafbbc171a
Welle77 <simon.wessmann.brok@gmail.com>
2021-09-22 10:51:29 +0200

    it works generic now

--- Assignment1/cbc-padding-oracle-task/encrypt.py
+++ Assignment1/cbc-padding-oracle-task/encrypt.py
@@ -1,70 +1,86 @@
 import requests
 from Crypto.Util.Padding import pad
+import math
 
 
-basepath = 'http://localhost:5000'
+#basepath = 'http://localhost:5000'
+basepath = 'https://cbc.syssec.lnrd.net'
 quotepath = basepath + '/quote'
 
-textToEncrypt = '<redacted>' + ' plain CBC is not secure!'
 
-def askPaddingOracle(text):
- cookies = {'authtoken': text.hex()}
- response = requests.get(quotepath, cookies = cookies)
- return response
+secretFromOracleAttack = "I should have used authenticated encryption because ..."
+textToEncrypt = secretFromOracleAttack + ' plain CBC is not secure!'
 
-def askForQuote(text):
- cookies = {'authtoken': text.hex()}
- response = requests.get(quotepath, cookies = cookies)
- print(response.text)
- return response
- 
+
+def askPaddingOracle(text: bytearray):
+    cookies = {'authtoken': text.hex()}
+    response = requests.get(quotepath, cookies=cookies)
+    return response
+
+
+def askForQuote(text: bytearray):
+    cookies = {'authtoken': text.hex()}
+    response = requests.get(quotepath, cookies=cookies)
+    print(response.text)
+    return response
+
+
 def encryptBlock(prev, next):
 
-   CipherBytes =  bytearray(16)
-   dBlock = bytearray(16)
-   cMarked = bytearray(16)
-   
+    CipherBytes = bytearray(16)
+    dBlock = bytearray(16)
+    cMarked = bytearray(16)
 
-   for byteindex in range (15,-1, -1): 
-      paddingvalue = 16 - byteindex
-      for index in range(15, byteindex, -1):
-         cMarked[index] = paddingvalue ^ dBlock[index]
-     
-      for value in range(0,256):
-         cMarked[byteindex] = value
-         blockToSend = cMarked + prev
-         
-         response = askPaddingOracle(blockToSend)
-         
-         if 'padding is incorrect' not in response.text.lower():
-            dByteValue = value ^ paddingvalue
-            dBlock[byteindex] = dByteValue
-            cipherByte = next[byteindex] ^ paddingvalue ^ value
-            CipherBytes[byteindex] = cipherByte
-            print(str(cipherByte))
-            break
-         else:
-            continue
+    for byteindex in range(15, -1, -1):
+        paddingvalue = 16 - byteindex
+        for index in range(15, byteindex, -1):
+            cMarked[index] = paddingvalue ^ dBlock[index]
 
-   return CipherBytes
+        for value in range(0, 256):
+            cMarked[byteindex] = value
+            blockToSend = cMarked + prev
 
- 
+            response = askPaddingOracle(blockToSend)
+
+            if 'padding is incorrect' not in response.text.lower():
+                dByteValue = value ^ paddingvalue
+                dBlock[byteindex] = dByteValue
+                cipherByte = next[byteindex] ^ paddingvalue ^ value
+                CipherBytes[byteindex] = cipherByte
+                break
+            else:
+                continue
+
+    return CipherBytes
+
 
 def main():
-   encodedText = textToEncrypt.encode()
-   textByteArray = bytearray(encodedText)
-   plainTextArray = pad(textByteArray, 16)
+    encodedText = textToEncrypt.encode()
+    textByteArray = bytearray(encodedText)
+    plainTextArray = pad(textByteArray, 16)
+    amountOfBlocks = math.ceil(len(plainTextArray)/16)
+    plainTextBlockArray = []
+    cipherTextBlockArray = []
+    lastCipherBlock = bytearray(16)
 
-   p1 = bytearray(plainTextArray[0:16])
-   p2 = bytearray(plainTextArray[16:32])
-   p3 = bytearray(plainTextArray[32:48])
+    for block in range(0, amountOfBlocks):
+        plainTextBlockArray.append(
+            bytearray(plainTextArray[16*block:16*block+16]))
 
-   c3 = bytearray(16)
-   c2 = encryptBlock(c3, p3)
-   c1 = encryptBlock(c2, p2)
-   iv = encryptBlock(c1, p1)
-   finalCipher = iv + c1 + c2 + c3
-   askForQuote(finalCipher)   
- 
+    cipherTextBlockArray.append(lastCipherBlock)
+
+    for index in range(amountOfBlocks - 1, -1, -1):
+
+        encryptedBlock = encryptBlock(
+            cipherTextBlockArray[0], plainTextBlockArray[index])
+        cipherTextBlockArray.insert(0, encryptedBlock)
+
+    blockToSend = bytearray()
+    for x in cipherTextBlockArray:
+        blockToSend += x
+
+    askForQuote(blockToSend)
+
+
 if __name__ == "__main__":
- main()
+    main()
--- Assignment1/cbc-padding-oracle-task/firstry.py
+++ Assignment1/cbc-padding-oracle-task/firstry.py
@@ -1,71 +1,84 @@
 import requests
-import secrets
+import math
 
-basepath = 'http://localhost:5000'
+basepath = 'https://cbc.syssec.lnrd.net/'
+#basepath = "http://localhost:5000"
 quotepath = basepath + '/quote'
 
-#Based on https://robertheaton.com/2013/07/29/padding-oracle-attack/
+# Based on https://robertheaton.com/2013/07/29/padding-oracle-attack/
+
 
 def extractTokenFrom(response):
- tokenHeader = response.cookies.get('authtoken')
- return tokenHeader
+    tokenHeader = response.cookies.get('authtoken')
+    return tokenHeader
+
 
 def askPaddingOracle(ciphertext):
- cookies = {'authtoken': ciphertext.hex()}
- response = requests.get(quotepath, cookies = cookies)
- return response
+    cookies = {'authtoken': ciphertext.hex()}
+    response = requests.get(quotepath, cookies=cookies)
+    return response
 
-#Decrypts a block of 16 bytes
+# Decrypts a block of 16 bytes
+
+
 def decryptBlock(previousBlock, blockToDecrypt):
- intermediateStateBytes = bytearray(16)
- decryptedPlainText = bytearray(16)
-    
- for byteindex in range(15, -1, -1):
-  paddingvalue = 16 - byteindex
-  sneakyPreviousBlock = bytearray(16)
-     
-  for index in range(15, byteindex, -1):
-   sneakyPreviousBlock[index] = paddingvalue ^ intermediateStateBytes[index]
-     
-  for value in range(0,256):
-   sneakyPreviousBlock[byteindex] = value
-   blockToSend = sneakyPreviousBlock + blockToDecrypt
-   
-   response = askPaddingOracle(blockToSend)
-   
-   if 'padding is incorrect' not in response.text.lower():
-    previousBlockValue = previousBlock[byteindex]
-    intermediateByteValue = value ^ paddingvalue
-    intermediateStateBytes[byteindex] = intermediateByteValue
-    plaintextByte = previousBlockValue ^ intermediateByteValue
-    decryptedPlainText[byteindex] = plaintextByte
-    print('Decrypted byte, the current block is now: ' + str(decryptedPlainText.decode().upper()))
-    break
-   else:
-    continue
- 
- print('Block is decrypted to be - ' + decryptedPlainText.decode().upper())
- return decryptedPlainText
+    intermediateStateBytes = bytearray(16)
+    decryptedPlainText = bytearray(16)
 
-#Assumes the ciphertext is 4 blocks of 16 bytes, could be made more generic
+    for byteindex in range(15, -1, -1):
+        paddingvalue = 16 - byteindex
+        sneakyPreviousBlock = bytearray(16)
+
+        for index in range(15, byteindex, -1):
+            sneakyPreviousBlock[index] = paddingvalue ^ intermediateStateBytes[index]
+
+        for value in range(0, 256):
+            sneakyPreviousBlock[byteindex] = value
+            blockToSend = sneakyPreviousBlock + blockToDecrypt
+
+            response = askPaddingOracle(blockToSend)
+
+            if 'padding is incorrect' not in response.text.lower():
+                previousBlockValue = previousBlock[byteindex]
+                intermediateByteValue = value ^ paddingvalue
+                intermediateStateBytes[byteindex] = intermediateByteValue
+                plaintextByte = previousBlockValue ^ intermediateByteValue
+                decryptedPlainText[byteindex] = plaintextByte
+                print('Decrypted byte, the current block is now: ' +
+                      str(decryptedPlainText.decode().upper()))
+                break
+            else:
+                continue
+
+    print('Block is decrypted to be - ' + decryptedPlainText.decode().upper())
+    return decryptedPlainText
+
+# Assumes the ciphertext is 4 blocks of 16 bytes, could be made more generic
+
+
 def runPOAttack(ciphertext):
- cipherbytes = (bytes.fromhex(ciphertext))
- 
- cipherblock1 = bytearray(cipherbytes[0:16])
- cipherblock2 = bytearray(cipherbytes[16:32])
- cipherblock3 = bytearray(cipherbytes[32:48])
- cipherblock4 = bytearray(cipherbytes[48:])
-    
- block4 = decryptBlock(cipherblock3, cipherblock4)
- block3 = decryptBlock(cipherblock2, cipherblock3)
- block2 = decryptBlock(cipherblock1, cipherblock2)
-    
- print('Successfully decrypted full ciphertext to be - ' + (block2 + block3 + block4).decode())
+    cipherbytes = (bytes.fromhex(ciphertext))
+
+    plaintext: str = ""
+    amountOfBlocks: int = math.ceil(len(cipherbytes)/16)
+
+    cipherBlockArray = []
+
+    for block in range(0, amountOfBlocks):
+        cipherBlockArray.append(bytearray(cipherbytes[16*block:16*block+16]))
+
+    for index in range(amountOfBlocks - 1, 0, -1):
+        block = decryptBlock(
+            cipherBlockArray[index - 1], cipherBlockArray[index])
+        plaintext = block.decode() + plaintext
+
+    print(plaintext)
+
 
 def main():
- ciphertext = extractTokenFrom(requests.get(basepath))
- runPOAttack(ciphertext)
+    ciphertext = extractTokenFrom(requests.get(basepath))
+    runPOAttack(ciphertext)
 
 
 if __name__ == "__main__":
- main()
+    main()
