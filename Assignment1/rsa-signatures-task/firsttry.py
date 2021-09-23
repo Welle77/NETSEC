a8eb797f8042a06899a35bda90972e88c76342b9
Jesper <repsej.stroem@gmail.com>
2021-09-23 13:57:42 +0200

    Works

--- Assignment1/rsa-signatures-task/firsttry.py
+++ Assignment1/rsa-signatures-task/firsttry.py
@@ -9,10 +9,14 @@ quotepath = basepath + '/quote'
 keypath = basepath + '/pk'
 signpath = basepath + '/sign_random_document_for_students'
 
-m = b'You got a 12 because you are an excellent student! :)'
-m1 = b'Rando message'
+#The message we want to have signed
+desiredMessage = 'You got a 12 because you are an excellent student! :)'.encode()
 
-# might be good explanation here https://crypto.stackexchange.com/questions/35644/chosen-message-attack-rsa-signature/35656
+#Set to decimal value 5, such that desiredmessage and randomMessage1 is divisible
+randomMessage1 = ''.encode()
+
+#Used for the random message attack
+randomMessage2 = 'This is a test'.encode()
 
 def tryGetQuote(message, signature):
  j = json.dumps({'msg': message, 'signature': signature})
@@ -36,36 +40,73 @@ def getGradeCookie():
 def buildSignPath(data):
  return signpath + '/' + data
 
-def main():
+def signRandomMessageAttack():
  N = json.loads(getPublicKey())['N']
- e = json.loads(getPublicKey())['e']
  
- m1Response = signMessage(m1)
+ m1Response = signMessage(randomMessage1)
  m1Data = json.loads(m1Response.text)
- 
- s1 = m1Data['signature'].encode()
- 
+ s1 = bytes.fromhex(m1Data['signature'])
+ m1 = bytes.fromhex(m1Data['msg'])
  s1Int = int.from_bytes(s1, 'big')
- 
- mInt = int.from_bytes(m, 'big')
  m1Int = int.from_bytes(m1, 'big')
  
- x = int((mInt*(1/m1Int)))
- m2Int = x % N
- 
- m2 = m2Int.to_bytes(math.ceil(m2Int.bit_length() / 8), 'big')
-
- m2Response = signMessage(m2)
- 
- s2 = json.loads(m2Response.text)['signature'].encode()
+ m2Response = signMessage(randomMessage2)
+ m2Data = json.loads(m2Response.text)
+ s2 = bytes.fromhex(m2Data['signature'])
+ m2 = bytes.fromhex(m2Data['msg'])
  s2Int = int.from_bytes(s2, 'big')
-
- sInt = (s1Int*s2Int) %N
+ m2Int = int.from_bytes(m2, 'big')
+ 
+ sInt = (s1Int * s2Int) % N
+ mInt = (m1Int * m2Int) % N
+ 
+ s = sInt.to_bytes(math.ceil(sInt.bit_length() /8),'big')
+ m = mInt.to_bytes(math.ceil(mInt.bit_length() /8),'big')
  
  mHex = m.hex()
- sHex = (sInt.to_bytes(math.ceil(sInt.bit_length() / 8), 'big')).hex()
+ sHex = s.hex()
  
- response = tryGetQuote(mHex, sHex)
+ return tryGetQuote(mHex, sHex).text
+
+def signDesiredMessageAttack():
+ N = json.loads(getPublicKey())['N']
+ 
+ m1Response = signMessage(randomMessage1)
+ m1Data = json.loads(m1Response.text)
+ s1 = bytes.fromhex(m1Data['signature'])
+ m1 = bytes.fromhex(m1Data['msg'])
+ s1Int = int.from_bytes(s1, 'big')
+ m1Int = int.from_bytes(m1, 'big')
+ 
+ mInt = int.from_bytes(desiredMessage, 'big')
+ 
+ #Needs to be a whole number, and therefore using // - ensures result is whole number
+ m2Int = mInt//m1Int % N
+ 
+ bytelength = math.ceil(m2Int.bit_length() /8)
+ m2Bytes = m2Int.to_bytes(bytelength, 'big')
+ 
+ m2Response = signMessage(m2Bytes)
+ m2Data = json.loads(m2Response.text)
+ m2Hex = m2Data['msg']
+ s2Hex = m2Data['signature']
+ s2 = bytes.fromhex(s2Hex)
+ m2 = bytes.fromhex(m2Hex)
+ 
+ s2Int = int.from_bytes(s2, 'big')
+ 
+ sInt = s1Int * s2Int % N
+ 
+ s = sInt.to_bytes(math.ceil(sInt.bit_length() / 8), 'big')
+ 
+ mHex = desiredMessage.hex()
+ sHex = s.hex()
+ 
+ return tryGetQuote(mHex, sHex).text
+
+def main():
+ print(signRandomMessageAttack())
+ print(signDesiredMessageAttack())
  
 if __name__ == "__main__":
  main()
