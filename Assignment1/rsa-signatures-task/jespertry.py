b3b196e8e5afb25a80e5866096c26e8a4c27256e
Andreas <holmern251@gmail.com>
2021-09-29 11:40:30 +0200

    YOLO

--- Assignment1/rsa-signatures-task/jespertry.py
+++ Assignment1/rsa-signatures-task/jespertry.py
@@ -1,7 +1,11 @@
 from Crypto.Hash import SHA256
 from Crypto.Signature.pss import MGF1
+from Crypto.Math import Primality
 import math
 import secrets
+import random
+
+system_random = random.SystemRandom()
 
 messageToSign = b'Hello world'
 
@@ -38,8 +42,11 @@ def EMSA_PSS_ENCODE(M, emBits):
   maskedDb[index] = DB[index] ^ dbMask[index]
  
  #NOT correct - should calculate which bits to set.
- byteValue = maskedDb[0]
- maskedDb[0] &= 0b01111111
+ numberOfBitsToClear = (8 * emLen) - emBits
+ x = 0b11111111
+ x = x >> numberOfBitsToClear
+ maskedDb[0] &= x
+ 
  bcByteArray = bytearray(1)
  bcByteArray[0] = 188
  EM = maskedDb + H + bcByteArray
@@ -49,8 +56,11 @@ def EMSA_PSS_ENCODE(M, emBits):
 def OS2IP(string):
  return int.from_bytes(string, 'big')
 
-def RSASP1(K: tuple, m):
- raise NotImplementedError
+def RSASP1(K, m):
+ n = K[0]
+ d = K[1]
+ assert 0 < m < (n - 1)
+ return m**d % n
 
 def I2OSP(integer):
  return integer.to_bytes(math.ceil(integer.bit_length() /8),'big')
@@ -60,8 +70,9 @@ def RSASSA_PSS_SIGN(K: tuple, M):
  encodedInteger = OS2IP(encodedMessage)
  signaturePrimitive = RSASP1(K, encodedInteger)
  signature = I2OSP(signaturePrimitive)
+ print(signature)
  return signature
-
+ 
 def RSASSA_PSS_VERIFY(pubK, M, S):
  raise NotImplementedError
 
@@ -71,13 +82,72 @@ def RSAVP1(pubK, s):
 
 def EMSA_PSS_VERIFY(M, EM, emBits):
  raise NotImplementedError
+
+def generate_keys(keySize):
+    p = generate_large_prime(keySize)
+    q = generate_large_prime(keySize)
+    n = p*q
+    
+    while True:
+        e = system_random.randrange(2**(keySize-1), 2**keySize)
+        if gcd(e,(p-1)*(q-1)) == 1:
+            break
+
+    d = findModInverse(e,(p-1)*(q-1))
+
+    publicKey = (n, e)
+    privateKey = (n, d)
+
+    return (privateKey, publicKey)
+
+def generate_large_prime(keySize):
+    while True:
+        num = system_random.randrange(2**(keySize-1), 2**keySize) # 2**keySize means 2^keySize
+        if Primality.test_probable_prime(num):
+            return num
+        
+        # if check_if_prime(num):
+        #     return num
+
+# def check_if_prime(num):
+#     # https://geeksforgeeks.org/python-program-to-check-whether-a-number-is-prime-or-not/
+#     if num > 1:
+#         for i in range(2, int(num/2)+1):
+#             if (num % i) == 0:
+#                 return False
+#             else:
+#                 return True
+#     else:
+#         return False
  
-def generateKeys():
- #Should generate valid keys somehow
- return (1,2), (3,4) 
+# region cryptomath module
+# http://inventwithpython.com/hacking (BSD Licensed)
+
+def gcd(a, b):
+    # Return the GCD of a and b using Euclid's Algorithm
+    while a != 0:
+        a, b = b % a, a
+    return b
+
+
+def findModInverse(a, m):
+    # Returns the modular inverse of a % m, which is
+    # the number x such that a*x % m = 1
+
+    if gcd(a, m) != 1:
+        return None # no mod inverse if a & m aren't relatively prime
+
+    # Calculate using the Extended Euclidean Algorithm:
+    u1, u2, u3 = 1, 0, a
+    v1, v2, v3 = 0, 1, m
+    while v3 != 0:
+        q = u3 // v3 # // is the integer division operator
+        v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
+    return u1 % m
+ # endregion
  
 def main():
- privKey, pubKey = generateKeys()
+ privKey, pubKey = generate_keys(modBits)
  signature = RSASSA_PSS_SIGN(privKey, messageToSign)
  print(signature)
  
